391b483a1371b524f8506d55dc87077d0997cad9
WellNotWell <olesya.gredyushko4956@gmail.com>
2023-07-01 17:14:37 +0300

    make getters for roles

--- Roles.py
+++ Roles.py
@@ -5,46 +5,46 @@ from aiogram import Bot
 
 class Citizen:
     def __init__(self, players):
-        self.players = players
+        self.__players = players
 
     async def notifyCitizens(self, bot: Bot):  # this method notify citizen players in game
-        for id in self.players:
+        for id in self.__players:
             await bot.send_message(chat_id=id, text="You are citizen")
 
     def getCitizensList(self):
-        return self.players
+        return self.__players
 
 
 class Mafia:
     def __init__(self, players):
-        self.players = players
-        print(self.players)
+        self.__players = players
+        print(self.__players)
 
     async def notifyMafias(self, bot: Bot):   # this method notify mafia players in game
-        for id in self.players:
+        for id in self.__players:
             await bot.send_message(chat_id=id, text="You are mafia")
 
     def getMafiaList(self):
-        return self.players
+        return self.__players
 
 
 class Detective:
     def __init__(self, player):     # player - id of the player
-        self.player = player
+        self.__player = player
 
     async def notifyDetective(self, bot: Bot):   # this method notify detective player in game
-        await bot.send_message(chat_id=self.player, text="You are detective")
+        await bot.send_message(chat_id=self.__player, text="You are detective")
 
     def getDetective(self):
-        return self.player
+        return self.__player
 
 
 class Doctor:
     def __init__(self, player):
-        self.player = player
+        self.__player = player
 
     async def notifyDoctor(self, bot: Bot):  # this method notify doctor player in game
-        await bot.send_message(chat_id=self.player, text="You are doctor")
+        await bot.send_message(chat_id=self.__player, text="You are doctor")
 
     def getDoctor(self):
-        return self.player
+        return self.__player
