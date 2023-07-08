de58e1a8ca599c0e875656aa9f4e0f9058629e2e
WellNotWell <olesya.gredyushko4956@gmail.com>
2023-07-08 16:00:15 +0300

    add necessary attributes and methods, change func for mafia players

--- Game.py
+++ Game.py
@@ -5,6 +5,25 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import Roles
 
 
+class Player:
+    def __init__(self, id, name):
+        self.__name = name
+        self.__id = id
+        self.__role = None
+
+    def getName(self):
+        return self.__name
+
+    def getId(self):
+        return self.__id
+
+    def setRole(self, role: Roles.Role):
+        self.__role = role
+
+    def getRole(self):
+        return self.__role
+
+
 class SignUpForTheGame:  # class implements registration of players
     def __init__(self):
         self.players = list()
@@ -30,6 +49,7 @@ class SignUpForTheGame:  # class implements registration of players
 
 class Game:
     def __init__(self):
+        self.__vote_count = None
         self.list_players = None
         self.bot = None
         self.chat_id = None
@@ -39,12 +59,17 @@ class Game:
         self.__doctor = None
         self.__detective = None
         self.__list_innocents = None
-        self.votes = {}
+        self.__killed_at_night = None
+        self.__killed_players = None
+        self.__votes = None
 
     async def setInfo(self, l: list, bot: Bot, chat_id):
         self.list_players = l  # ids of players
         self.bot = bot  # bot from telegram
         self.chat_id = chat_id
+        self.__vote_count = 0
+        self.__votes = {}
+        self.__killed_players = []
 
     async def start_game(self):
         self.time_of_day = "night"  # flag that defines day cycle
@@ -53,18 +78,28 @@ class Game:
     async def nightCycle(self):
         self.setNight()
         await self.bot.send_message(chat_id=self.chat_id, text="The night is coming. "
-                                                               "The city falls asleep, the mafia wakes up.")
+                                                               "The city falls asleep")
+
+        await self.bot.send_message(chat_id=self.chat_id, text="Mafia is waking up. They are choosing their victim")
         await self.showVoteToKill()
 
     # actions for day cycle
     async def dayCycle(self):
         self.setDay()
-        await self.bot.send_message(chat_id=self.chat_id, text="Mafia has made its choice -_-")
-        await self.bot.send_message(chat_id=self.chat_id, text="It's daytime. Discuss and vote for the Mafia.")
+        if self.__killed_at_night is not None:
+            await self.bot.send_message(chat_id=self.__killed_at_night.getId(), text="You were killed by mafia")
+            await self.bot.send_message(chat_id=self.chat_id, text=f"Player {self.__killed_at_night.getName()} "
+                                                                   f"was killed that night. His role is "
+                                                                   f"{self.__killed_at_night.getRole().value}")
+        else:
+            await self.bot.send_message(chat_id=self.chat_id, text="Nobody died this night.")
+
+        self.resetNightVictimMafia()
+        await self.bot.send_message(chat_id=self.chat_id, text="It's daytime. Discuss and vote for the Mafia")
 
     # for Mafia players to choose a victim
     async def showVoteToKill(self):
-        self.votes = {player: 0 for player in self.__list_innocents}
+        self.__votes = {player: 0 for player in self.__list_innocents}
         choosePlayers = InlineKeyboardMarkup(row_width=len(self.__list_innocents))
         for player in self.__list_innocents:
             username = player.getName()
@@ -77,9 +112,11 @@ class Game:
             await self.bot.send_message(chat_id=member.getId(), text="Choose a player to kill",
                                         reply_markup=choosePlayers)
 
-    async def getChatMemberIdByUsername(self, username):
-        chat_member = await self.bot.get_chat_member(chat_id=self.chat_id, user_id=username)
-        return chat_member.user.id
+    async def getChatMemberByUsername(self, username):
+        for player in self.__list_innocents:
+            if player.getName() == username:
+                return player
+        return None
 
     def setDay(self):
         self.time_of_day = "day"
@@ -87,8 +124,10 @@ class Game:
     def setNight(self):
         self.time_of_day = "night"
 
-    def killPlayer(self, id):  # method for kill citizen
-        self.list_players.remove(id)
+    def killPlayer(self, player):  # method for kill citizen
+        self.list_players.remove(player)
+        self.__killed_at_night = player
+        self.__killed_players.append(player)
 
     async def defineRoles(self):
         # DEFINE MAFIAS PLAYERS
@@ -103,46 +142,74 @@ class Game:
 
         indexes_mafia_players = random.sample(range(len(self.list_players)),
                                               k=number_of_mafia)  # choose random players to be mafia
-        list_mafia_id = [self.list_players[i] for i in indexes_mafia_players]  # list of player's id to write them msgs
+        list_mafia = [self.list_players[i] for i in indexes_mafia_players]  # list of mafia players
+        [player.setRole(Roles.Role.MAFIA) for player in list_mafia]
 
-        mafia = Roles.Mafia(list_mafia_id)  # Create instance of class mafia and put ids mafia players
+        mafia = Roles.Mafia(list_mafia)  # Create instance of class mafia and put ids mafia players
         await mafia.notifyMafias(self.bot)  # notify players
         self.__mafia = mafia
         # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-        civilians = list(set(self.list_players).difference(set(list_mafia_id)))  # list of players without mafia
+        civilians = list(set(self.list_players).difference(set(list_mafia)))  # list of players without mafia
         self.__list_innocents = list(civilians)  # take list of innocents
 
         if len(self.list_players) > 3:
-            doctor_id = random.choice(civilians)
-            doctor = Roles.Doctor(doctor_id)  # Create doctor and notify player
+            doctor = random.choice(civilians)
+            doctor.setRole(Roles.Role.DOCTOR)
+            doctor = Roles.Doctor(doctor)  # Create doctor and notify player
             await doctor.notifyDoctor(self.bot)
             self.__doctor = doctor
 
-            civilians.remove(doctor_id)  # list of players without mafia and doctor
+            civilians.remove(doctor)  # list of players without mafia and doctor
 
         if len(self.list_players) > 5:
-            detective_id = random.choice(civilians)
-            detective = Roles.Detective(detective_id)  # Create detective and notify player
+            detective = random.choice(civilians)
+            detective.setRole(Roles.Role.DETECTIVE)
+            detective = Roles.Detective(detective)  # Create detective and notify player
             await detective.notifyDetective(self.bot)
             self.__detective = detective
 
-            civilians.remove(detective_id)  # list of citizens
+            civilians.remove(detective)  # list of citizens
 
+        [player.setRole(Roles.Role.CITIZEN) for player in civilians]
         citizens = Roles.Citizen(civilians)  # Create citizens
         await citizens.notifyCitizens(self.bot)  # Notify players
         self.__citizens = citizens
 
         # We have to complete defines roles
 
+    def getMafia(self):
+        return self.__mafia
 
-class Player:
-    def __init__(self, id, name):
-        self.__name = name
-        self.__id = id
+    def getPlayers(self):
+        return self.list_players
 
-    def getName(self):
-        return self.__name
+    def getKilledPlayers(self):
+        return self.__killed_players
 
-    def getId(self):
-        return self.__id
+    def updateVotes(self, player: Player):
+        self.__votes[player] += 1
+
+    def updateVoteCount(self):
+        self.__vote_count += 1
+
+    def resetVoteCount(self):
+        self.__vote_count = 0
+
+    def getVoteCount(self):
+        return self.__vote_count
+
+    def getVotes(self):
+        return self.__votes
+
+    def resetVotes(self):
+        self.__votes = {}
+
+    def getChatId(self):
+        return self.chat_id
+
+    def getBot(self):
+        return self.bot
+
+    def resetNightVictimMafia(self):
+        self.__killed_at_night = None
