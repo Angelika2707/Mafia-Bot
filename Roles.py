de3a499909f78d94856fbad5d34e6cd6392e1ca5
WellNotWell <olesya.gredyushko4956@gmail.com>
2023-07-08 15:55:55 +0300

    add enum with role types

--- Roles.py
+++ Roles.py
@@ -1,7 +1,15 @@
 from aiogram import Bot
-
+from enum import Enum
 
 # file contains roles in mafia
+
+
+class Role(Enum):
+    CITIZEN = 'citizen'
+    DETECTIVE = 'detective'
+    MAFIA = 'mafia'
+    DOCTOR = 'doctor'
+
 
 class Citizen:
     def __init__(self, players):
@@ -9,7 +17,7 @@ class Citizen:
 
     async def notifyCitizens(self, bot: Bot):  # this method notify citizen players in game
         for player in self.__players:
-            await bot.send_message(chat_id=player.getId(), text="You are citizen")
+            await bot.send_message(chat_id=player.getId(), text=f"You are {Role.CITIZEN.value}")
 
     def getCitizensList(self):
         return self.__players
@@ -20,20 +28,20 @@ class Mafia:
         self.__players = players
         print(self.__players)
 
-    async def notifyMafias(self, bot: Bot):   # this method notify mafia players in game
+    async def notifyMafias(self, bot: Bot):  # this method notify mafia players in game
         for player in self.__players:
-            await bot.send_message(chat_id=player.getId(), text="You are mafia")
+            await bot.send_message(chat_id=player.getId(), text=f"You are {Role.MAFIA.value}")
 
     def getMafiaList(self):
         return self.__players
 
 
 class Detective:
-    def __init__(self, player):     # player - id of the player
+    def __init__(self, player):  # player - id of the player
         self.__player = player
 
-    async def notifyDetective(self, bot: Bot):   # this method notify detective player in game
-        await bot.send_message(chat_id=self.__player.getId(), text="You are detective")
+    async def notifyDetective(self, bot: Bot):  # this method notify detective player in game
+        await bot.send_message(chat_id=self.__player.getId(), text=f"You are {Role.DETECTIVE.value}")
 
     def getDetective(self):
         return self.__player
@@ -44,7 +52,7 @@ class Doctor:
         self.__player = player
 
     async def notifyDoctor(self, bot: Bot):  # this method notify doctor player in game
-        await bot.send_message(chat_id=self.__player.getId(), text="You are doctor")
+        await bot.send_message(chat_id=self.__player.getId(), text=f"You are {Role.DOCTOR.value}")
 
     def getDoctor(self):
         return self.__player
